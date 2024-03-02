9b71db34a10cbae05aea83483ea8813feb4a0455
Tripti Jain <148478310+TriptiJainTJ@users.noreply.github.com>
2024-03-02 17:09:36 +0530

    Delete DotNet directory

--- DotNet/pro3/pro3/pro3/DAL/Service/IMailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmailApplication.Model;
-
-namespace EmailApplication.Interface
-{
-    public interface IMailService
-    {
-        public Task SendEmailAsync(MailRequest mailrequest);
-    }
-}
